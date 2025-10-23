2e56197df85a5c600932268fa2cad0433398f14e
marcaumdesign <navalservice1@gmail.com>
2025-10-23 19:44:02 -0300

    Refactor Hero component to improve performance and update styling for better responsiveness

---
+++ app/test-3/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { HeroTest3 } from "@/components/hero-test-3";
+import { Leva } from "leva";
+
+export default function Home() {
+  return (
+    <>
+      <HeroTest3 />
+      <Leva hidden />
+    </>
+  );
+}
